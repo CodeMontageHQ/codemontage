6e4d2a2319c16ebad0718b90146bd5fe6ff78087
Vanessa Hurst <vrhurst@gmail.com>
2013-01-23 16:54:25 -0500

    add organization link to project admin

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Project do
-  
+  index do
+    column "Organization", :sortable => :organization_name do |proj|
+      link_to proj.organization.name, admin_organization_path(proj.organization)
+    end
+    
+    column :name
+    column :github_repo
+    column :description
+    column :notes
+    column :created_at
+    column :updated_at
+  end  
 end
