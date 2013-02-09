4f8bb6dc7422c575f3da600b1048374366152849
Vanessa Hurst <vrhurst@gmail.com>
2013-02-09 17:42:33 -0500

    visit user dashboard after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    projects_path
+    dashboard_path
   end
   
   def after_sign_out_path_for(resource_or_scope) #return to page you logged out from
