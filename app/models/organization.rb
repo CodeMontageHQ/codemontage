a0880aa71f646270395b84ceb25e41d729e263dc
Vanessa Hurst <vrhurst@gmail.com>
2013-02-12 01:22:36 -0500

    show twitter url for organizations where available

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,10 +1,14 @@
 class Organization < ActiveRecord::Base
   has_many :projects
   
-  attr_accessible :name, :url, :github_org, :description, :is_tax_exempt, :contact_name, :contact_role, :contact_email, :annual_budget_usd, :total_staff_size, :tech_staff_size, :notes, :image_url
+  attr_accessible :name, :url, :github_org, :description, :is_tax_exempt, :contact_name, :contact_role, :contact_email, :annual_budget_usd, :total_staff_size, :tech_staff_size, :notes, :image_url, :twitter
 
   def github_url
     github_url = ("http://github.com/" + self.github_org) unless self.github_org.blank?
+  end
+
+  def twitter_url
+    twitter_url = ("http://twitter.com/" + self.twitter) unless self.twitter.blank?
   end
   
 end
