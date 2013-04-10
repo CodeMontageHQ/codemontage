cae472e4a77808181bdaf08e35dab496584101c1
Vanessa Hurst <vrhurst@gmail.com>
2013-04-10 03:18:21 -0400

    direct to projects instead of dashboard on sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    dashboard_path
+    projects_path
   end
   
   def after_sign_out_path_for(resource_or_scope) #return to page you logged out from
