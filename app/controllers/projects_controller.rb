235f7ab07ce41c46abb32b2c9ebd45ad81378705
Vanessa Hurst <vrhurst@gmail.com>
2013-06-28 21:29:58 -0400

    refactor featured project logic from controller to model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   def index
-    @featured_projects = Project.where("organization_id IS NOT NULL")
+    @featured_projects = Project.featured
   end
 
   def show
