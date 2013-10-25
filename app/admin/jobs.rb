87487c80361633d21fd49d88b7031682f1d62797
Vanessa Hurst <vrhurst@gmail.com>
2013-10-25 17:40:08 -0400

    add causes & technologies to job posts

--- app/admin/jobs.rb
+++ app/admin/jobs.rb
@@ -9,10 +9,43 @@ ActiveAdmin.register Job do
     column "Application Link", :sortable => :apply_url do |job|
       link_to job.apply_url
     end
-    column :expires_at
+    column "Causes", :cause_list
+    column "Technologies", :technology_list
+    column "Expires", :expires_at
     column :created_at
     column :updated_at
     
     default_actions
   end
+
+  form do |f|
+    f.inputs "Job Post", :multipart => true do    
+      f.input :organization
+      f.input :title
+      f.input :overview
+      f.input :apply_url
+      f.input :cause_list, :label => "Causes"
+      f.input :technology_list, :label => "Technologies"
+      f.input :expires_at, :as => :datepicker, :label => "Expires"
+    end
+    
+    f.buttons
+  end
+
+  show do |ad|
+    attributes_table do
+      row :organization
+      row :title
+      row :overview
+      row :apply_url
+      row "Causes" do ad.cause_list end
+      row "Technologies" do ad.technology_list end
+      row "Expires" do ad.expires_at end
+      row :created_at
+      row :updated_at
+    end
+    
+    active_admin_comments
+  end
+
 end
