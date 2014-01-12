0b96e85a8b7fb2dbe9fe7872aabdf9f3ac20e0fd
Vanessa Hurst <vrhurst@gmail.com>
2014-01-12 04:50:42 -0500

    use aws s3 for paperclip images in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,4 +68,14 @@ CodeMontage::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Add-ins
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
