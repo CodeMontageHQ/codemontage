db9197a977bd63c4c32ac42e4740ca5d1e6d6a93
Vanessa Hurst <vrhurst@gmail.com>
2014-01-13 02:09:07 -0500

    take users to dashboard when they log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :force_www!
 
   def after_sign_in_path_for(resource)
-    projects_path
+    dashboard_path
   end
 
   def after_sign_out_path_for(resource_or_scope) #return to page you logged out from
