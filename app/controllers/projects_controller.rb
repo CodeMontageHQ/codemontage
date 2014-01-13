86b0fa56a83b0424afa1b5102cedb6ade8954335
Vanessa Hurst <vrhurst@gmail.com>
2014-01-12 22:56:27 -0500

    fix project favoriting behavior for logged out users

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,10 +2,11 @@ class ProjectsController < ApplicationController
 
   def index
     @featured_projects = Project.featured
-    @favorite_projects = FavoriteProject.
-      where(:user_id => current_user.id).
-      map {|p| p.project_id }.
-      to_set
+    @favorite_projects = 
+      if current_user.present?
+        FavoriteProject.where(:user_id => current_user.id).map {|p| p.project_id }.to_set
+      else
+      end
   end
 
   def show
