07e2ccb22d30a5758aaa08301131d8175c5c58fc
Vanessa Hurst <vrhurst@gmail.com>
2014-01-13 00:18:16 -0500

    add tag filtering on projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,12 @@
 class ProjectsController < ApplicationController
 
   def index
-    @featured_projects = Project.featured
+    @featured_projects = 
+      if params[:tags].present?
+        Project.featured.tagged_with(params[:tags], :any => true) 
+      else 
+        Project.featured 
+      end
     @favorite_projects = 
       if current_user.present?
         FavoriteProject.where(:user_id => current_user.id).map {|p| p.project_id }.to_set
