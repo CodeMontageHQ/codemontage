e25df898aa1d2c329c6d50ad75688427b769bf24
Vanessa Hurst <vrhurst@gmail.com>
2014-01-15 03:30:38 -0500

    show event and user names in event registration admin

--- app/admin/event_registrations.rb
+++ app/admin/event_registrations.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register EventRegistration do
-  
+  index do
+    column "Event", :sortable => :event_name do |registration|
+      link_to registration.event.name, admin_event_path(registration.event)
+    end
+    column "User", :sortable => :user_name do |registration|
+      link_to registration.user.name, admin_user_path(registration.user)
+    end
+    column :created_at
+    column :updated_at
+    
+    default_actions
+  end
+
+  filter :event_id
+  filter :user_id, :label => "User ID", :as => :numeric
+  filter :created_at
+  filter :updated_at
+    
 end
