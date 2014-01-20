59ebb6e517613d375156594def38879107d6165e
Patrik Schlund <patrik.schlund@gmail.com>
2014-01-20 18:06:39 -0500

    Fix for issue 135, Do not require GitHub Organization for orgs created via Admin

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -7,6 +7,7 @@ class OrganizationsController < ApplicationController
 
 	def create
 		@organization = Organization.new(params[:organization])
+		@organization.is_public_user = true
 
 		if @organization.save
 			redirect_to root_path, :notice => "Your project has been submitted for approval."
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,5 +4,18 @@ describe Organization do
   it { should have_many(:organization_metrics) }
   it { should have_many(:projects) }
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:github_org) }
+
+  context 'if public user' do
+    before { subject.stub(:is_public_user){true} }
+    it { should validate_presence_of(:github_org) }
+  end
+
+  context 'if not public user' do
+    before { subject.stub(:is_public_user){false} }
+    it { should_not validate_presence_of(:github_org) }
+  end
+
+  context 'if not public user, implicit' do
+    it { should_not validate_presence_of(:github_org) }
+  end
 end
