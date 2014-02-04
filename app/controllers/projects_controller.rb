c33553b72afdc2957924c4aab60fa0fe99e7dbc0
steverob <stevanity.hf@gmail.com>
2014-02-04 12:51:33 +0530

    preloaded technology tags and added a helper to render them out as list of links in projects/index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,8 @@ class ProjectsController < ApplicationController
         Project.featured.tagged_with(params[:tags], :any => true) 
       else 
         Project.featured 
-      end
+      end.includes :technologies
+
     @favorite_projects = 
       if current_user.present?
         FavoriteProject.where(:user_id => current_user.id).map {|p| p.project_id }.to_set
--- app/helpers/project_controller_helper.rb
+++ app/helpers/project_controller_helper.rb
@@ -5,4 +5,10 @@ module ProjectControllerHelper
     code_later_end = code_later_date + "T180000Z"  
     return "https://www.google.com/calendar/render?action=TEMPLATE&text=Code+with+CodeMontage&dates=" + code_later_start + "/" + code_later_end + "&details=For+ideas,+visit+http://codemontage.com/projects.&location=Anywhere&sf=true&output=xml"
   end
+
+  def technology_list_links project
+    project.technologies.map do |tech|
+      link_to tech.name, projects_path(:tags => tech.name)
+    end.join(", ").html_safe
+  end
 end
