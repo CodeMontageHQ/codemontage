fbe38e94e39a7a49bbdd8750be3217afbc55cee2
steverob <stevanity.hf@gmail.com>
2014-02-13 01:00:51 +0530

    preload organizations in jobs page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,7 @@
 class JobsController < ApplicationController
 
   def index
-    @active_jobs = Job.active
+    @active_jobs = Job.active.with_organization
     @hiring_orgs = Organization.hiring
   end
 end
