6ce300cbd69696e11a2540b9a9dce25e5bfeaaa1
Vanessa Hurst <vrhurst@gmail.com>
2014-05-14 14:56:49 -0400

    enable entire event creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ActiveRecord::Base
   has_many :event_registrations
   has_many :users, :through => :event_registrations
   
-  attr_accessible :name, :start_date, :end_date, :description, :notes
+  attr_accessible :name, :short_code, :start_date, :end_date, :description, :notes
 end
