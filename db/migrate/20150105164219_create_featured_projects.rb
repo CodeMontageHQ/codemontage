70a790e291e709f0bb3ec3efe64528589e772cf4
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-05 12:45:36 -0500

    Adjust migration to use Ruby 1.9 hash syntax

--- db/migrate/20150105164219_create_featured_projects.rb
+++ db/migrate/20150105164219_create_featured_projects.rb
@@ -1,8 +1,8 @@
 class CreateFeaturedProjects < ActiveRecord::Migration
   def change
     create_table :featured_projects do |t|
-      t.references :project, :null => false
-      t.references :event, :null => false
+      t.references :project, null: false
+      t.references :event, null: false
 
       t.timestamps
     end
