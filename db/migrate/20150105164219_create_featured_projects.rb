1e3f24f32d1ded30bfce73c3d9daf9464e51dc15
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-05 12:27:07 -0500

    Create migration for featured projects, with index on event

---
+++ db/migrate/20150105164219_create_featured_projects.rb
@@ -0,0 +1,12 @@
+class CreateFeaturedProjects < ActiveRecord::Migration
+  def change
+    create_table :featured_projects do |t|
+      t.references :project, :null => false
+      t.references :event, :null => false
+
+      t.timestamps
+    end
+
+    add_index :featured_projects, :event_id
+  end
+end
