7daa8d365f88ed5be047a318bd8316d3dfce842a
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-09 08:55:31 -0500

    Allow FeaturedProject validation to work without db finagling

--- spec/models/featured_project_spec.rb
+++ spec/models/featured_project_spec.rb
@@ -4,7 +4,10 @@ describe FeaturedProject do
   it { should belong_to(:event) }
   it { should belong_to(:project) }
 
-  it { should validate_uniqueness_of(:project_id).scoped_to(:event_id) }
+  describe "validations" do
+    subject { create(:featured_project) }
+    it { should validate_uniqueness_of(:project_id).scoped_to(:event_id) }
+  end
 
   it { should allow_mass_assignment_of(:project_id) }
   it { should allow_mass_assignment_of(:event_id) }
