280ba90c7687284a2d01555ebf47d774897abcde
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-12 23:25:05 -0500

    Adjust Github pr & commit service methods to stipulate for repo

--- app/services/github.rb
+++ app/services/github.rb
@@ -1,14 +1,14 @@
 module Github
   module_function
 
-  def pull_requests(org, repo, day_begin, day_end)
+  def pull_requests_by_repo(org, repo, day_begin, day_end)
     Octokit.auto_paginate = true
     results = Octokit.search_issues "repo:#{org_repo(org, repo)} type:pr created:#{day_begin}..#{day_end}"
     results.items
   end
 
-  def commits(org, repo, day_begin, day_end)
-    prs = pull_requests(org, repo, day_begin, day_end)
+  def commits_by_repo(org, repo, day_begin, day_end)
+    prs = pull_requests_by_repo(org, repo, day_begin, day_end)
     commits = prs.map do |pr|
       Octokit.pull_commits org_repo(org, repo), pr.number
     end
