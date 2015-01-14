4cf68a62adaba2df1c910f00993db8e83d55d568
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-14 16:18:33 -0500

    Remove unused event_registration factory

--- spec/factories/event_registrations.rb
+++ spec/factories/event_registrations.rb
@@ -8,9 +8,4 @@ FactoryGirl.define do
     association :event, factory: :october
     association :user, factory: :user_with_github
   end
-
-  factory :er_for_jan_wk1, class: EventRegistration do
-    association :event, factory: :jan_wk1
-    association :event, factory: :user_with_github
-  end
 end
