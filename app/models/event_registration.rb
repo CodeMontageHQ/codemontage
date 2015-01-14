67293d7a808c77efb427037e3499450a59ca172b
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-14 15:31:19 -0500

    Return user stats from event_registrations

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -4,6 +4,30 @@ class EventRegistration < ActiveRecord::Base
 
   attr_accessible :event_id, :user_id
 
+  def stats
+    if github_stat_args
+      args = github_stat_args
+      results = {}
+
+      results[:prs]     = Github.pull_requests_by_user(args[:user],
+                                                       args[:day_begin],
+                                                       args[:day_end]).count
+
+      results[:issues]  = Github.issues_by_user(args[:user], args[:day_begin],
+                                                args[:day_end]).count
+
+      results[:commits] = Github.commits_by_user(args[:user], args[:day_begin],
+                                                 args[:day_end]).count
+
+      results[:forks]   = Github.forks_by_user(args[:user], args[:day_begin],
+                                               args[:day_end]).count
+
+      results
+    else
+      nil
+    end
+  end
+
   def github_stat_args
     if user.has_github
       uid = user.services.find_by_provider(:github).uid
