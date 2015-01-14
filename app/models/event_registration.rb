88c5388cfe7a4306db44c0d4225dbdb3106e9d4e
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-14 15:22:49 -0500

    Add EventRegistration method to create args for Github methods

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -3,4 +3,16 @@ class EventRegistration < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :event_id, :user_id
+
+  def github_stat_args
+    if user.has_github
+      uid = user.services.find_by_provider(:github).uid
+
+      { user:      Github.get_user_login_by_uid(uid),
+        day_begin: event.start_date,
+        day_end:   event.end_date }
+    else
+      nil
+    end
+  end
 end
