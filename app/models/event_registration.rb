f34de4722cba22fa4a1e8b62dbbd1b34540a3e07
Courteney Ervin <courteney.ervin@gmail.com>
2015-01-23 17:20:52 -0500

    Allow event_registrations to return and use their own #github_api_args (with specs!)

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -5,34 +5,15 @@ class EventRegistration < ActiveRecord::Base
   attr_accessible :event_id, :user_id
 
   def stats
-    if github_stat_args
-      args = github_stat_args
+    if github_api_args
+      args = github_api_args
       github_client = Github.new
       results = {}
 
-      results[:prs]     = github_client.pull_requests_by_user(
-                            args[:user],
-                            args[:day_begin],
-                            args[:day_end]
-                          ).count
-
-      results[:issues]  = github_client.issues_by_user(
-                            args[:user],
-                            args[:day_begin],
-                            args[:day_end]
-                          ).count
-
-      results[:commits] = github_client.commits_by_user(
-                            args[:user],
-                            args[:day_begin],
-                            args[:day_end]
-                          ).count
-
-      results[:forks]   = github_client.forks_by_user(
-                            args[:user],
-                            args[:day_begin],
-                            args[:day_end]
-                          ).count
+      results[:prs]     = github_client.pull_requests_by_user(args).count
+      results[:issues]  = github_client.issues_by_user(args).count
+      results[:commits] = github_client.commits_by_user(args).count
+      results[:forks]   = github_client.forks_by_user(args).count
 
       results
     else
@@ -40,13 +21,10 @@ class EventRegistration < ActiveRecord::Base
     end
   end
 
-  def github_stat_args
-    if user.has_github
-      uid = user.services.find_by_provider(:github).uid
-
-      { user:      Github.new.get_user_login_by_uid(uid),
-        day_begin: event.start_date,
-        day_end:   event.end_date }
+  def github_api_args
+    if user.github_api_args
+      user.github_api_args.merge!(day_begin: event.start_date,
+                                  day_end: event.end_date)
     else
       nil
     end
--- spec/models/event_registration_spec.rb
+++ spec/models/event_registration_spec.rb
@@ -4,12 +4,12 @@ describe EventRegistration do
   it { should belong_to(:event) }
   it { should belong_to(:user) }
 
-  describe "#github_stat_args" do
+  describe "#github_api_args" do
 
     context "without github auth" do
       it "returns nil" do
         er = create(:event_registration)
-        expect(er.github_stat_args).to be_nil
+        expect(er.github_api_args).to be_nil
       end
     end
 
@@ -18,9 +18,11 @@ describe EventRegistration do
         er = create(:er_for_october)
 
         VCR.use_cassette("courte_user") do
-          expect(er.github_stat_args).to eq(user: "courte",
-                                            day_begin: er.event.start_date,
-                                            day_end: er.event.end_date)
+          expect(er.github_api_args).to eq(
+            user: "courte",
+            day_begin: Time.new(2014, 10, 01),
+            day_end: Time.new(2014, 10, 31)
+          )
         end
       end
     end
@@ -38,7 +40,7 @@ describe EventRegistration do
       it "returns metrics in a hash", github_api: true do
         er = create(:er_for_october)
 
-        VCR.use_cassette("courte_oct_stats") do
+        VCR.use_cassette("courte_er_stats") do
           expect(er.stats).to eq(prs: 1, issues: 0, commits: 1, forks: 2)
         end
       end
