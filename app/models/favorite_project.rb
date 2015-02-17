1d37569360baa5aac04a8dd53d4faa0b7315d1a1
Courteney Ervin <courteney.ervin@gmail.com>
2015-02-17 00:50:00 -0500

    Return faves created between two specific dates

--- app/models/favorite_project.rb
+++ app/models/favorite_project.rb
@@ -3,4 +3,8 @@ class FavoriteProject < ActiveRecord::Base
   belongs_to :user
 
   validates_uniqueness_of :project_id, :scope => :user_id
+
+  def self.created_between(start_date, end_date)
+    where('created_at >= ? AND created_at <= ?', start_date, end_date)
+  end
 end
