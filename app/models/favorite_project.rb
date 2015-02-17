b8e64e87db785064b1890b5d4bea54afd0fefd75
Courteney Ervin <courteney.ervin@gmail.com>
2015-02-17 12:11:00 -0500

    Adjust syntax according to HoundCI recommendations

--- app/models/favorite_project.rb
+++ app/models/favorite_project.rb
@@ -5,6 +5,6 @@ class FavoriteProject < ActiveRecord::Base
   validates_uniqueness_of :project_id, :scope => :user_id
 
   def self.created_between(start_date, end_date)
-    where('created_at >= ? AND created_at <= ?', start_date, end_date)
+    where("created_at >= ? AND created_at <= ?", start_date, end_date)
   end
 end
