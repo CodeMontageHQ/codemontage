e49faf880128f01164c53fe57ee96fd94508d008
Courteney Ervin <courteney.ervin@gmail.com>
2015-03-31 23:35:32 -0400

    Adjust settings for Heroku

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -10,12 +10,12 @@ CodeMontage::Application.configure do
   # Mandrill smtp settings
   config.action_mailer.smtp_settings = {
     user_name: ENV["MANDRILL_USERNAME"],
-    password: ENV["MANDRILL_PASSWORD"],
+    password: ENV["MANDRILL_APIKEY"],
     address: "smtp.mandrillapp.com",
     port: 587,
     enable_starttls_auto: true,
     authentication: :plain,
-    domain: "codemontage.com"
+    domain: "heroku.com"
   }
 
   # Code is not reloaded between requests
